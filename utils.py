bc1be3b07079e7e17858c01651b681ab5ccfc1d7
AhnYoungBin <49204525+AhnYoungBin@users.noreply.github.com>
2020-05-09 11:20:23 +0900

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,34 @@
+import torch.nn.functional as F
+import torch
+import torch.nn as nn
+class DiceBCELoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(DiceBCELoss, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1):
+        
+        inputs = F.sigmoid(inputs)       
+        bce_weight = 0.5
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        intersection = (inputs * targets).sum()                            
+        dice_loss = 1 - (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)  
+        BCE = F.binary_cross_entropy(inputs, targets, reduction='mean')
+        loss_final = BCE * bce_weight + dice_loss * (1 - bce_weight)
+        return loss_final
+
+class DiceScore(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(DiceScore, self).__init__()
+
+    def forward(self, inputs, targets, smooth=1):
+        
+        inputs = F.sigmoid(inputs)       
+        
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        intersection = (inputs * targets).sum()                            
+        dice_score = (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth) 
+        return dice_score 
