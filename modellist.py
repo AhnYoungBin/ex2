00fbc0d477bf9bc7583b45fcce6063d41a5ee0ad
AhnYoungBin <49204525+AhnYoungBin@users.noreply.github.com>
2020-05-09 12:07:39 +0900

    Update modellist.py

--- modellist.py
+++ modellist.py
@@ -3,14 +3,14 @@ from model import Unet, fcn_resnet
 class Modellist():
     def __init__(self):
         print('modelnum list')
-        print('-'*30)
+        print('-'*100)
         print('1: Unet')
         print('2: fcn_resnet')
         print('3: x')
-        print('-'*30)
+        print('-'*100)
 
     def __call__(self,x):
-        return {1: Unet.Unet(1),
-        2: fcn_resnet.fcn_resnet.FCN()
+        return {1: Unet.UNet(1),
+        2: fcn_resnet.FCN()
         #3: Vgg.VGG16(seon)
-        }
+        }[x]
