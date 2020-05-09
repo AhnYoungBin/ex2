1c69194acf38b235d29952045e0a3dac32c6e09e
AhnYoungBin <49204525+AhnYoungBin@users.noreply.github.com>
2020-05-09 12:08:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,43 @@
 #main.py
 import torch
 import albumentations as A
-from torchvision import transforms
 from torch.utils.data import random_split, DataLoader
 import numpy as np
-import matplotlib.pyplot as plt
-#from data import Brain_DataSet
+import data
+import modellist
+import argparse
+import utils
+import torch.optim as optim
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 path ='/content/gdrive/My Drive/lgg-mri-segmentation/kaggle_3m'
+
+modellist = modellist.Modellist()
+
+parser = argparse.ArgumentParser(description='Learn by Modeling Segmentation DataSet')
+parser.add_argument('modelnum',type=int, help='Select your model number')
+parser.add_argument("-show", help="show to model Archtecture",action="store_true")
+parser.add_argument('lr',type=float, help='Select opimizer learning rate')
+parser.add_argument('epochs',type=int, help='Select train epochs')
+args = parser.parse_args()
+
+model = modellist(args.modelnum)
+model = model.to(device)
+criterion = utils.DiceBCELoss()
+optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=0.9, weight_decay=5e-4)
+print('criterion : DiceBCELoss')
+print('learning_rate : ',args.lr)
+print('optimizer : SGD(momentum =0.9 , decay = 5e-4')
+print('-'*100)
+
+#show to model archtecture
+if args.show:
+    print('Model Archtecture')
+    print('-'*100)
+    print(model)
+    print('-'*100)
+
 
 albumentations_transform = A.Compose([
     A.Resize(256,256),
@@ -18,7 +47,7 @@ albumentations_transform = A.Compose([
     A.HorizontalFlip(p=0.5)
 ])
 
-dataset = Brain_DataSet(path,albumentations_transform)
+dataset = data.Brain_DataSet(path,albumentations_transform)
 sample_image, sample_mask = dataset[0]
 print('데이터셋 크기: ',len(dataset))
 print('이미지 크기 : ',sample_image.size())
@@ -31,12 +60,6 @@ print('검증 데이터셋 크기: ',len(val_dataset))
 
 train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=16,shuffle=True)
 val_loader = torch.utils.data.DataLoader(dataset=val_dataset, batch_size=16)
-
-model = UNet(1).to(device)
-criterion = DiceBCELoss()
-learning_rate = 1e-5
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-
 
 train_loss = []
 val_loss = []
@@ -78,8 +101,8 @@ def val():
     print('Validation loss: {}'.format(epoch_val_loss))                                
     val_loss.append(epoch_val_loss)
 
-for epoch in range(1,26):
-    print('Epoch {}/{}'.format(epoch, 25))
+for epoch in range(1,args.epochs+1):
+    print('Epoch {}/{}'.format(epoch, args.epochs))
     print('-'*20)
     train()
     val()
